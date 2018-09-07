488266efd0e178a7bad12e04448fb74a7139bc4a
Twometer <twometer@outlook.de>
2018-09-07 16:36:35 +0200

    Added main functionality

--- QuickJS/GUI/MainWindow.xaml.cs
+++ QuickJS/GUI/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using ICSharpCode.AvalonEdit.Highlighting;
+﻿using ICSharpCode.AvalonEdit.AddIn;
+using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Indentation.CSharp;
+using ICSharpCode.SharpDevelop.Editor;
+using Noesis.Javascript;
+using QuickJS.JavaScript;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,10 +27,64 @@ namespace QuickJS
     /// </summary>
     public partial class MainWindow : Window
     {
+        private JsExecutor jsExecutor = new JsExecutor();
+
         public MainWindow()
         {
             InitializeComponent();
+            jsExecutor.Initialize((s) =>
+            {
+                ConsoleTextBox.AppendText(s + "\n");
+            });
             textEditor.TextArea.IndentationStrategy = new CSharpIndentationStrategy(textEditor.Options);
+            InitializeTextMarkerService();
+        }
+
+        ITextMarkerService textMarkerService;
+
+        void InitializeTextMarkerService()
+        {
+            var textMarkerService = new TextMarkerService(textEditor.Document);
+            textEditor.TextArea.TextView.BackgroundRenderers.Add(textMarkerService);
+            textEditor.TextArea.TextView.LineTransformers.Add(textMarkerService);
+            IServiceContainer services = (IServiceContainer)textEditor.Document.ServiceProvider.GetService(typeof(IServiceContainer));
+            if (services != null)
+                services.AddService(typeof(ITextMarkerService), textMarkerService);
+            this.textMarkerService = textMarkerService;
+        }
+
+        private int FindIndex(int row)
+        {
+            var line = 0;
+            var index = 0;
+            foreach (var str in textEditor.Text.Split('\n'))
+            {
+                if (row == line)
+                    return index;
+                line++;
+                index += str.Length;
+            }
+            throw new IndexOutOfRangeException();
+        }
+
+        private void RunButton_Click(object sender, RoutedEventArgs e)
+        {
+            textMarkerService.RemoveAll(m => true);
+            try
+            {
+                ConsoleTextBox.Foreground = Brushes.Black;
+                ConsoleTextBox.Clear();
+                jsExecutor.Execute(textEditor.Text);
+            }
+            catch (JavascriptException ex)
+            {
+                ConsoleTextBox.Foreground = Brushes.Red;
+                ConsoleTextBox.AppendText($"line {ex.Line} col {ex.StartColumn}: {ex.Message}");
+                var lineIndex = FindIndex(ex.Line - 1);
+                ITextMarker marker = textMarkerService.Create(lineIndex + ex.StartColumn, ex.EndColumn - ex.StartColumn);
+                marker.MarkerTypes = TextMarkerTypes.SquigglyUnderline;
+                marker.MarkerColor = Colors.Red;
+            }
         }
     }
 }
---
+++ QuickJS/JavaScript/JsExecutor.cs
@@ -0,0 +1,32 @@
+﻿using Noesis.Javascript;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickJS.JavaScript
+{
+    public class JsExecutor
+    {
+        private JavascriptContext javascriptContext;
+        private Action<string> callback;
+
+        public void Initialize(Action<string> consoleCallback)
+        {
+            Console console = new Console();
+            console.Listener = consoleCallback;
+            callback = consoleCallback;
+            javascriptContext = new JavascriptContext();
+            javascriptContext.SetParameter("console", console);
+        }
+
+        public void Execute(string script)
+        {
+            var i = javascriptContext.Run(script);
+            if (i != null)
+                callback(i.ToString());
+        }
+
+    }
+}
